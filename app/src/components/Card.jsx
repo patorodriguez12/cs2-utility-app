ecfd48ea426424779580808f4149ace6e3628744
patorodriguez12 <pato_rodriguez12@outlook.com>
2025-07-02 19:48:55 -0300

    testing how to start

--- app/src/components/Card.jsx
+++ app/src/components/Card.jsx
@@ -1,3 +1,12 @@
-export default function Card() {
-  return <div><h2>This is a Card</h2></div>;
-}
+import styles from "../styles/Card.module.css";
+
+const Card = ({ name, image, onClick }) => {
+  return (
+    <div className={styles.card} onClick={onClick}>
+      <img src={image} alt={name} className={styles.mapImage} />
+      <h3 className={styles.mapName}>{name}</h3>
+    </div>
+  );
+};
+
+export default Card;
--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -1,15 +1,61 @@
-export default function Home() {
+import React from "react";
+import Card from "../components/Card"
+import styles from "../styles/Home.module.css"
+
+const maps = [
+  {
+    name: "Mirage",
+    image: "./assets/mirage.webp",
+    logo: "./assets/mirage-logo.webp",
+  },
+  {
+    name: "Dust 2",
+    image: "./assets/dust2.webp",
+    logo: "./assets/dust2-logo.webp",
+  },
+  {
+    name: "Nuke",
+    image: "./assets/nuke.webp",
+    logo: "./assets/nuke-logo.webp",
+  },
+  {
+    name: "Inferno",
+    image: "./assets/inferno.webp",
+    logo: "./assets/inferno-logo.webp",
+  },
+  {
+    name: "Anubis",
+    image: "./assets/anubis.webp",
+    logo: "./assets/anubis-logo.webp",
+  },
+  {
+    name: "Ancient",
+    image: "./assets/ancient.webp",
+    logo: "./assets/ancient-logo.webp",
+  },
+];
+
+const Home = () => {
+  const handleClick = (mapName) => {
+    // Aquí redirigirías a la página de detalles del mapa
+    console.log(`Navegar a los detalles de ${mapName}`);
+  };
+
   return (
-    <div>
-      <h1>TIPS PARA LOS PIBES</h1>
-      <ul>
-        <li>Mirage</li>
-        <li>Dust 2</li>
-        <li>Nuke</li>
-        <li>Inferno</li>
-        <li>Anubis</li>
-        <li>Ancient</li>
-      </ul>
+    <div className={styles.homeContainer}>
+      <h1 className={styles.title}>Mapas de Counter Strike 2</h1>
+      <div className={styles.mapList}>
+        {maps.map((map) => (
+          <Card
+            key={map.name}
+            name={map.name}
+            image={map.image}
+            onClick={() => handleClick(map.name)}
+          />
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
+
+export default Home;

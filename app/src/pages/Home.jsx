9d55541b207897c524507eff85b1e5c8b4d0d51b
patorodriguez12 <pato_rodriguez12@outlook.com>
2025-07-03 18:28:00 -0300

    modularizacion de home, ahora la data de los maps esta en otro archivo

--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -1,39 +1,6 @@
-import React from "react";
 import Card from "../components/Card";
 import styles from "../styles/Home.module.css";
-
-const maps = [
-  {
-    name: "Mirage",
-    image: "https://i.imgur.com/xo39TBZ.png",
-    logo: "https://i.imgur.com/uUP10aO.png",
-  },
-  {
-    name: "Dust 2",
-    image: "https://i.imgur.com/MiqrSGw.png",
-    logo: "https://i.imgur.com/jOEmf42.png",
-  },
-  {
-    name: "Nuke",
-    image: "https://i.imgur.com/u2ekRvE.png",
-    logo: "https://i.imgur.com/AwBLhSM.png",
-  },
-  {
-    name: "Inferno",
-    image: "https://i.imgur.com/NiouVuk.png",
-    logo: "https://i.imgur.com/HTKJUMY.png",
-  },
-  {
-    name: "Anubis",
-    image: "https://i.imgur.com/r97TdZt.png",
-    logo: "https://i.imgur.com/qx7FMIw.png",
-  },
-  {
-    name: "Ancient",
-    image: "https://i.imgur.com/Y8xyA6z.png",
-    logo: "https://i.imgur.com/h1PXjXe.png",
-  },
-];
+import { maps } from "../utils/maps";
 
 const Home = () => {
   const handleClick = (mapName) => {
---
+++ app/src/utils/maps.js
@@ -0,0 +1,38 @@
+export const maps = [
+  {
+    id: 1,
+    name: "Mirage",
+    image: "https://i.imgur.com/xo39TBZ.png",
+    logo: "https://i.imgur.com/uUP10aO.png",
+  },
+  {
+    id: 2,
+    name: "Dust 2",
+    image: "https://i.imgur.com/MiqrSGw.png",
+    logo: "https://i.imgur.com/jOEmf42.png",
+  },
+  {
+    id: 3,
+    name: "Nuke",
+    image: "https://i.imgur.com/u2ekRvE.png",
+    logo: "https://i.imgur.com/AwBLhSM.png",
+  },
+  {
+    id: 4,
+    name: "Inferno",
+    image: "https://i.imgur.com/NiouVuk.png",
+    logo: "https://i.imgur.com/HTKJUMY.png",
+  },
+  {
+    id: 5,
+    name: "Anubis",
+    image: "https://i.imgur.com/r97TdZt.png",
+    logo: "https://i.imgur.com/qx7FMIw.png",
+  },
+  {
+    id: 6,
+    name: "Ancient",
+    image: "https://i.imgur.com/Y8xyA6z.png",
+    logo: "https://i.imgur.com/h1PXjXe.png",
+  },
+];

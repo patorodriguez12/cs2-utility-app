6bff93443f43ca452f40d16437e925bb311a7792
patorodriguez12 <pato_rodriguez12@outlook.com>
2025-07-03 19:19:13 -0300

    data para hacer unas pruebas

---
+++ app/src/utils/tips.js
@@ -0,0 +1,8 @@
+export const tips = [
+  {
+    id: 1,
+    mapId: "mirage",
+    title: "Tip 1 para mirage",
+    videoUrl: "...",
+  },
+];
---
+++ app/src/utils/types.js
@@ -0,0 +1,22 @@
+export const types = [
+  {
+    id: "flashbang",
+    name: "Fbs",
+    imgUrl: "...",
+  },
+  {
+    id: "smoke",
+    name: "Humos",
+    imgUrl: "...",
+  },
+  {
+    id: "he",
+    name: "Detos",
+    imgUrl: "...",
+  },
+  {
+    id: "molotov",
+    name: "Molos",
+    imgUrl: "...",
+  },
+];

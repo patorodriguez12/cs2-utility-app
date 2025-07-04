e459a390d91bc4b337525477fb5bf7b1878121bc
patorodriguez12 <pato_rodriguez12@outlook.com>
2025-07-04 11:08:44 -0300

    normalizando estilos para que sean mas consistentes

--- app/src/pages/MapDetail.jsx
+++ app/src/pages/MapDetail.jsx
@@ -15,23 +15,25 @@ const MapDetail = () => {
   };
 
   return (
-    <div className={styles.typeGrid}>
-      
-      {types.map((type) => (
-        <div
-          key={type.id}
-          className={styles.typeCard}
-          onClick={() => handleTipTypeClick(type.id)}
-        >
-          <img
-            src={type.background}
-            alt={type.name}
-            className={styles.background}
-          />
-          <img src={type.imgUrl} alt={type.name} />
-          <span className={styles.typeName}>{type.name}</span>
-        </div>
-      ))}
+    <div className={styles.typeContainer}>
+      <h1 className={styles.title}>Elegi la utilidad</h1>
+      <div className={styles.typeList}>
+        {types.map((type) => (
+          <div
+            key={type.id}
+            className={styles.typeCard}
+            onClick={() => handleTipTypeClick(type.id)}
+          >
+            <img
+              src={type.background}
+              alt={type.name}
+              className={styles.background}
+            />
+            <img src={type.imgUrl} alt={type.name} />
+            <span className={styles.typeName}>{type.name}</span>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
